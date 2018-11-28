7e694dbbf94dbc2724931681eb46fc1385807ae9
Geoffrey Moss <geoffrey.moss@wheelergmail.org>
2018-11-28 17:46:11 -0500

    Started work on GUI, making Observer methods

--- src/place/client/gui/PlaceGUI.java
+++ src/place/client/gui/PlaceGUI.java
@@ -2,18 +2,42 @@ package place.client.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+import place.PlaceBoard;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class PlaceGUI extends Application implements Observer {
+
+    private PlaceBoard board;
+
+    @Override
+    public void init() {
+        //TODO connect to server
+
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        BorderPane border = new BorderPane();
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(border);
         primaryStage.show();
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        assert arg == this.board : "Update called by something that isn't the model";
+
+        this.refresh();
+    }
+
+    public void refresh() {
         //TODO
     }
 
