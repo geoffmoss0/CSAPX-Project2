2ed14d72bb55f0400052932b3f726f8bade04eb9
Aaron Jiang <aj6566@rit.edu>
2018-11-28 17:59:45 -0500

    Added in ConsoleApplication and edited PTUI so it has abstract of CA

--- src/place/client/ptui/PlacePTUI.java
+++ src/place/client/ptui/PlacePTUI.java
@@ -1,9 +1,42 @@
 package place.client.ptui;
 
+import place.PlaceBoard;
+import place.PlaceColor;
+import place.PlaceException;
+import place.PlaceTile;
+
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
-public class PlacePTUI implements Observer {
+public class PlacePTUI extends ConsoleApplication implements Observer {
+
+    private PlaceBoard board;
+
+    private PlaceColor color;
+
+    private PlaceTile tile;
+
+    private PlaceException exception;
+
+    @Override
+    public void init(){
+        //TODO
+    }
+
+    @Override
+    public synchronized void go (Scanner userIn, PrintWriter userOut){
+        //TODO
+    }
+
+    @Override
+    public void stop(){
+        //TODO
+    }
+
+
     @Override
     public void update(Observable o, Object arg) {
 
@@ -11,6 +44,10 @@ public class PlacePTUI implements Observer {
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Usage: java PlaceClient host port username");
+            System.exit(0);
+        }
+        else{
+            ConsoleApplication.launch(PlacePTUI.class, args);
         }
     }
 }
